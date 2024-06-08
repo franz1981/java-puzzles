d1f5db418c66bf040df23eef71fd0d293a1996d2
Francesco Nigro <nigro.fra@gmail.com>
2024-06-08 16:09:56 +0200

    Need escaping benchmark

---
+++ src/main/java/red/hat/puzzles/string/CheckEscapingString.java
@@ -0,0 +1,183 @@
+package red.hat.puzzles.string;
+
+import org.openjdk.jmh.annotations.Benchmark;
+import org.openjdk.jmh.annotations.BenchmarkMode;
+import org.openjdk.jmh.annotations.Fork;
+import org.openjdk.jmh.annotations.Measurement;
+import org.openjdk.jmh.annotations.Mode;
+import org.openjdk.jmh.annotations.OutputTimeUnit;
+import org.openjdk.jmh.annotations.Scope;
+import org.openjdk.jmh.annotations.Setup;
+import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.annotations.Warmup;
+
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.VarHandle;
+import java.nio.ByteOrder;
+import java.util.concurrent.TimeUnit;
+
+@State(Scope.Benchmark)
+@Warmup(iterations = 10, time = 400, timeUnit = TimeUnit.MILLISECONDS)
+@Measurement(iterations = 5, time = 200, timeUnit = TimeUnit.MILLISECONDS)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@BenchmarkMode(Mode.AverageTime)
+@Fork(2)
+public class CheckEscapingString {
+
+    // TODO: optimize this for both archs
+    private static final VarHandle LONG = MethodHandles.byteArrayViewVarHandle(long[].class, ByteOrder.BIG_ENDIAN);
+    private static final int ESCAPE_STANDARD = -1;
+    /**
+     * Lookup table used for determining which output characters in
+     * 7-bit ASCII range need to be quoted.
+     */
+    protected final static int[] sOutputEscapes128;
+
+    static {
+        int[] table = new int[128];
+        // Control chars need generic escape sequence
+        for (int i = 0; i < 32; ++i) {
+            table[i] = ESCAPE_STANDARD;
+        }
+        // Others (and some within that range too) have explicit shorter sequences
+        table['"'] = '"';
+        table['\\'] = '\\';
+        // Escaping of slash is optional, so let's not add it: these are still within the previous [0-31] range!
+        table[0x08] = 'b';
+        table[0x09] = 't';
+        table[0x0C] = 'f';
+        table[0x0A] = 'n';
+        table[0x0D] = 'r';
+        sOutputEscapes128 = table;
+    }
+
+    String input;
+    byte[] output;
+
+    @Setup
+    public void init() {
+        input = "1234567812345678";
+        output = new byte[input.length()];
+    }
+
+    @Benchmark
+    public boolean needEscapeBranchy() {
+        return needEscapeBranchy(input, output);
+    }
+
+
+    @Benchmark
+    public boolean needEscapeBranchless() {
+        return needEscapeBranchless(input, output);
+    }
+
+    private static boolean needEscapeBranchy(String input, byte[] output) {
+        for (int i = 0; i < input.length(); i++) {
+            int ch = input.charAt(i);
+            if (ch > 0x7f || sOutputEscapes128[ch] != 0) {
+                return true;
+            }
+            output[i] = (byte) ch;
+        }
+        return false;
+    }
+
+    private static boolean needEscapeBranchless(String chars, byte[] output) {
+        int batches = chars.length() / 8;
+        int off = 0;
+        for (int i = 0; i < batches; i++) {
+            final long batch1 = (((long) chars.charAt(off)) << 48) |
+                    (((long) chars.charAt(off + 2)) << 32) |
+                    chars.charAt(off + 4) << 16 |
+                    chars.charAt(off + 6);
+            final long batch2 = (((long) chars.charAt(off + 1)) << 48) |
+                    (((long) chars.charAt(off + 3)) << 32) |
+                    chars.charAt(off + 5) << 16 |
+                    chars.charAt(off + 7);
+            // pack the 8 bytes into a long, regardless
+            long asciiValues = (batch1 << 8) | batch2;
+            // from now one we reason like the bytes are all ascii
+            // this is checking if we're withing the [0, 31] range: any negative value here have the 8th bit set
+            long lessThan32 = asciiValues - 0x2020202020202020L;
+            // the xor makes sure that we have 0 only for the bytes we search for: subtracting 1 will make the 8th bit set
+            // table['"'] = '"'
+            // NOTE on myself of the future: C2 can understand math and this trick to overflow isn't happening like this, on x86!
+            long notQuote = (asciiValues ^ 0x2222222222222222L) - 0x0101010101010101L;
+            // table['\\'] = '\\'
+            long notBackslash = (asciiValues ^ 0x5C5C5C5C5C5C5C5CL) - 0x0101010101010101L;
+            // filtering only the most significant bit of each byte
+            // check if there's any non-ASCII char it will be in the high bytes
+            long nonAscii = (batch1 | batch2) & 0xff80_ff80_ff80_ff80L;
+            boolean toEscape = (nonAscii | ((lessThan32 | notQuote | notBackslash) & 0x8080808080808080L )) != 0;
+            if (toEscape) {
+                return true;
+            }
+            if (LONG != null) {
+                LONG.set(output, off, asciiValues);
+            } else {
+                // copy back the bytes from asciiValues to output
+                output[off] = (byte) (asciiValues >> 56);
+                output[off + 1] = (byte) (asciiValues >> 48);
+                output[off + 2] = (byte) (asciiValues >> 40);
+                output[off + 3] = (byte) (asciiValues >> 32);
+                output[off + 4] = (byte) (asciiValues >> 24);
+                output[off + 5] = (byte) (asciiValues >> 16);
+                output[off + 6] = (byte) (asciiValues >> 8);
+                output[off + 7] = (byte) asciiValues;
+            }
+            off += 8;
+        }
+        // TODO let's be smart with the tail here!!!
+        return false;
+    }
+
+    /*
+    private static boolean needEscapeBranchless(String chars, byte[] output) {
+        int batches = chars.length() / 8;
+        int off = 0;
+        for (int i = 0; i < batches; i++) {
+            final long batch1 = (((long) chars.charAt(off)) << 48) |
+                    (((long) chars.charAt(off + 2)) << 32) |
+                    chars.charAt(off + 4) << 16 |
+                    chars.charAt(off + 6);
+            final long batch2 = (((long) chars.charAt(off + 1)) << 48) |
+                    (((long) chars.charAt(off + 3)) << 32) |
+                    chars.charAt(off + 5) << 16 |
+                    chars.charAt(off + 7);
+            // pack the 8 bytes into a long, regardless
+            long asciiValues = (batch1 << 8) | batch2;
+            // from now one we reason like the bytes are all ascii
+            // this is checking if we're withing the [0, 31] range: any negative value here have the 8th bit set
+            long notSpecialChars = asciiValues - 0x2020202020202020L;
+            // the xor makes sure that we have 0 only for the bytes we search for: subtracting 1 will make the 8th bit set
+            // table['"'] = '"'
+            notSpecialChars |= (asciiValues ^ 0x2222222222222222L) - 0x0101010101010101L;
+            // table['\\'] = '\\'
+            notSpecialChars |= (asciiValues ^ 0x5C5C5C5C5C5C5C5CL) - 0x0101010101010101L;
+            // filtering only the most significant bit of each byte
+            notSpecialChars &= 0x8080808080808080L;
+            // check if there's any non-ASCII char it will be in the high bytes
+            long nonAscii = (batch1 | batch2) & 0xff80_ff80_ff80_ff80L;
+            boolean toEscape = (nonAscii | notSpecialChars) != 0;
+            if (toEscape) {
+                return true;
+            }
+            if (LONG != null) {
+                LONG.set(output, off, asciiValues);
+            } else {
+                // copy back the bytes from asciiValues to output
+                output[off] = (byte) (asciiValues >> 56);
+                output[off + 1] = (byte) (asciiValues >> 48);
+                output[off + 2] = (byte) (asciiValues >> 40);
+                output[off + 3] = (byte) (asciiValues >> 32);
+                output[off + 4] = (byte) (asciiValues >> 24);
+                output[off + 5] = (byte) (asciiValues >> 16);
+                output[off + 6] = (byte) (asciiValues >> 8);
+                output[off + 7] = (byte) asciiValues;
+            }
+            off += 8;
+        }
+        // TODO let's be smart with the tail here!!!
+        return false;
+    }*/
+}
