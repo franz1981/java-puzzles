66b587b89ee351cd91d65db0c21dc873cb74acd8
Francesco Nigro <nigro.fra@gmail.com>
2025-09-26 07:35:38 +0200

    Better (more correct) explanation

--- src/main/java/red/hat/puzzles/polymorphism/TypeProfileInvokeVirtual.java
+++ src/main/java/red/hat/puzzles/polymorphism/TypeProfileInvokeVirtual.java
@@ -25,8 +25,13 @@ abstract class Base {
 
 /**
  * This benchmark has been created to understand patterns like the one at https://github.com/franz1981/netty/blob/6984ba79327c8d9e2c1d6c4d9cd3304f845021c3/common/src/main/java/io/netty/util/Recycler.java#L306
- * i.e. passing a known concrete type as a parameter of an inlineable method, expecting the call site to become
- * monomorphic, despite the method itself has been compiled with a type profile containing multiple types.
+ * i.e. passing a known concrete type as a parameter of an inlineable method, expecting the call site to become monomorphic
+ * despite the method itself has previously compiled with a mega-morphic call-site.
+ *
+ * This benchmark can be run with -XX:CICompilerCount=1 -XX:-UseOnStackReplacement -XX:-TieredCompilation -XX:-BackgroundCompilation -jar
+ * target/benchmarks.jar red.hat.puzzles.polymorphism.TypeProfileInvokeVirtual -f 0
+ *
+ * for debugging purposes
  */
 
 @BenchmarkMode(Mode.AverageTime)
@@ -70,7 +75,7 @@ public class TypeProfileInvokeVirtual extends Base {
         return b.base() % m;
     }
 
-    @Param({"false", "true"})
+    @Param({"true"})
     public boolean polluteTypeProfile;
 
     private TypeProfileInvokeVirtual concreteTypeInstance;
@@ -84,50 +89,63 @@ public class TypeProfileInvokeVirtual extends Base {
         concreteTypeInstance = this;
         abstractTypeInstance = concreteTypeInstance;
         instances[0] = concreteTypeInstance;
+        B b = new B();
+        C c = new C();
+        D d = new D();
         if (polluteTypeProfile) {
-            instances[1] = new B();
-            instances[2] = new C();
-            instances[3] = new D();
+            instances[1] = b;
+            instances[2] = c;
+            instances[3] = d;
         } else {
-            instances[1] = instances[0];
-            instances[2] = instances[0];
-            instances[3] = instances[0];
+            instances[1] = concreteTypeInstance;
+            instances[2] = concreteTypeInstance;
+            instances[3] = concreteTypeInstance;
         }
         // let's get doModulus compiled with a type profile containing 1 or 4 types in
-        for (int warmup = 0; warmup < 100_000; warmup++) {
+        for (int warmup = 0; warmup < 11_000; warmup++) {
             for (int i = 0; i < instances.length; i++) {
                 bh.consume(doModulus(instances[i], params[i]));
             }
         }
+        System.out.println("WARMUP COMPLETED!");
     }
 
     /**
-     * doModulus contains a virtual call to Base.base() and during the warmup with type pollution
-     * it's compiled with a type profile information on Base::base which contains 4 types.
-     * Once modulusSpeculativeTypeStaticFinal is hot enough and is compiled, the doModulus call-site is evaluated for inlining
-     * and its type profile information for Base::base is overridden by the speculative type A of its parameter.
-     * <p>
-     * - on https://github.com/openjdk/jdk/blob/2aafda1968f3fc8902f7d146a1cba72998aeb976/src/hotspot/share/opto/doCall.cpp#L674
-     * the doModulus parameter type is propagated as speculative type: this happens in the caller
-     * (modulusSpeculativeTypeStaticFinal) compilation once the doModulus call-site is processed
-     * - on https://github.com/openjdk/jdk/blob/2aafda1968f3fc8902f7d146a1cba72998aeb976/src/hotspot/share/opto/doCall.cpp#L195-L214 is where the
-     * inlining decision to inline doModulus into modulusSpeculativeTypeStaticFinal is made: it performs a deferred inlining decision
-     * - on https://github.com/openjdk/jdk/blob/2aafda1968f3fc8902f7d146a1cba72998aeb976/src/hotspot/share/opto/callGenerator.cpp#L524 is where the
-     * refinement logic is applied while inlining doModulus into modulusSpeculativeTypeStaticFinal
-     * i.e. it (re)uses the speculative type context creating a refined inlined version of doModulus
-     * - on https://github.com/openjdk/jdk/blob/2aafda1968f3fc8902f7d146a1cba72998aeb976/src/hotspot/share/opto/doCall.cpp#L237-L246
-     * the speculative type is used to treat the call to Base::base in the inlined version of doModulus as monomorphic
-     * <p>
-     * NOTE:
-     * Why inlining is deferred?
-     * Hotspot perform incremental inlining:
-     * 1. to better exploit contextual type information
-     * 2. avoid node explosion
-     * 3. spread inlining work across compilation phases to reduce the memory pressure
-     * <p>
-     * But it doesn't mean the timing to happen in a separate time from the caller compilation, just delayed
-     * later in the same process e.g. there's no need of "additional" invocations of a method to trigger inlining.
+     * We end up optimizing Base::base during the compilation of this method, while inlining doModulus and the call to Base::base
+     * <pre>
+     * Parse::do_call doCall.cpp:588                    <------ before optimizing virtual call Base::base it passes the runtime receiverNode
+     * Parse::do_one_bytecode parse2.cpp:2723
+     * Parse::do_one_block parse1.cpp:1593
+     * Parse::do_all_blocks parse1.cpp:724
+     * Parse::Parse parse1.cpp:628                      <------ doModulus
+     * ParseGenerator::generate callGenerator.cpp:97
+     * Parse::do_call doCall.cpp:677
+     * Parse::do_one_bytecode parse2.cpp:2723
+     * Parse::do_one_block parse1.cpp:1593
+     * Parse::do_all_blocks parse1.cpp:724
+     * Parse::Parse parse1.cpp:628                      <------ modulusSpeculativeTypeStaticFinal
+     * ParseGenerator::generate callGenerator.cpp:97
+     * Compile::Compile compile.cpp:806
+     * C2Compiler::compile_method c2compiler.cpp:147
+     * CompileBroker::invoke_compiler_on_method compileBroker.cpp:2345
+     * CompileBroker::compiler_thread_loop compileBroker.cpp:1989
+     * CompilerThread::thread_entry compilerThread.cpp:69
+     * JavaThread::thread_main_inner javaThread.cpp:775
+     * JavaThread::run javaThread.cpp:760
+     * Thread::call_run thread.cpp:243
+     * thread_native_entry os_linux.cpp:898
+     *</pre>
+     * and will end up into https://github.com/openjdk/jdk/blob/b73663a2b4fe7049fc0990c1a1e51221640b4e29/src/hotspot/share/opto/doCall.cpp#L1185
+     * which is making through the CHA analysis to determine that the call is monomorphic.
      *
+     * The key point is that https://github.com/openjdk/jdk/blob/b73663a2b4fe7049fc0990c1a1e51221640b4e29/src/hotspot/share/opto/doCall.cpp#L587-L598
+     * found the concrete type {@code TypeProfileInvokeVirtual} in the receiver_node while performing do_call of base
+     * in the inlined doModulus context:
+     * <pre>
+     *     Node* receiver_node             = stack(sp() - nargs);
+     * </pre>
+     * This information is then passed from optimize_virtual_call to optimize_inlining till
+     * the mentioned https://github.com/openjdk/jdk/blob/b73663a2b4fe7049fc0990c1a1e51221640b4e29/src/hotspot/share/opto/doCall.cpp#L1185.
      */
     @Benchmark
     @CompilerControl(CompilerControl.Mode.DONT_INLINE)
@@ -135,31 +153,18 @@ public class TypeProfileInvokeVirtual extends Base {
         return doModulus(SINGLETON_CONCRETE_TYPE_INSTANCE, 3);
     }
 
-    /**
-     * Same as modulusSpeculativeTypeStaticFinal but it needs to read the instance field out from the benchmark state
-     */
     @Benchmark
     @CompilerControl(CompilerControl.Mode.DONT_INLINE)
     public int modulusSpeculativeTypeInstanceField() {
         return doModulus(concreteTypeInstance, 3);
     }
 
-    /**
-     * Same as modulusSpeculativeTypeInstanceField although this should be passed as a register parameter
-     * and not read from an instance field
-     */
     @Benchmark
     @CompilerControl(CompilerControl.Mode.DONT_INLINE)
     public int modulusSpeculativeTypeThis() {
         return doModulus(this, 3);
     }
 
-    /**
-     * It perform differently based if type pollution happens:
-     * - if no type pollution, doModulus is compiled with a type profile containing only A: fast
-     * - if type pollution, doModulus is compiled with a type profile containing 4 types,
-     * but although base is always an A at runtime, the call to Base::base() is not treated as monomorphic
-     */
     @Benchmark
     @CompilerControl(CompilerControl.Mode.DONT_INLINE)
     public int modulusNoSpeculativeTypeInstanceField() {
